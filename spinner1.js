dfd0965fe23206d96f872c640930bc66be7fa0e2
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-15 23:40:02 +0000

    fixed code to be spinning full circle

--- spinner1.js
+++ spinner1.js
@@ -33,11 +33,6 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   ');
-}, 1500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   ');
-}, 1700);
+}, 1500);
 // ... fill in the rest yourself ...
